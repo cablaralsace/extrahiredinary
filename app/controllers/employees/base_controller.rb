82097facf86d908c922e23f53e3a9f699e9215eb
Alsace <alsace@narralabs.com>
2023-03-03 17:16:01 +0800

    Add Employee layout, index, navbar views

--- app/controllers/employees/base_controller.rb
+++ app/controllers/employees/base_controller.rb
@@ -1,3 +1,5 @@
 class Employees::BaseController < ApplicationController
   before_action :authenticate_employee!
+
+  layout "employee"
 end
---
+++ app/controllers/employees/dashboard_controller.rb
@@ -0,0 +1,4 @@
+class Employees::DashboardController < Employees::BaseController
+  def index
+  end
+end
