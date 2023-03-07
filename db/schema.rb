8e28d1d33077b79934bab765ac9d0139a5fd4434
Alsace <alsace@narralabs.com>
2023-03-07 16:40:04 +0800

    Implement STI on applicant model and add devise views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_071051) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_021951) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_071051) do
     t.text "character_references"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
     t.index ["email"], name: "index_employees_on_email", unique: true
     t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
   end
