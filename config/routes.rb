0486a850a10c6609f6b1c071754c73ed64008937
Alsace <alsace@narralabs.com>
2023-03-08 14:01:35 +0800

    Cleanup code

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # root to: "home#index"
   devise_scope :user do
     authenticated do
       resources :users
@@ -16,8 +12,4 @@ Rails.application.routes.draw do
       root to: "devise/sessions#new"
     end
   end
-  # namespace :user do
-  #   root :to => "users#index"
-  # end
-
 end
