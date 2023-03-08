56010ae0bb57685dc86c625fa8fbc741621c3528
Alsace <alsace@narralabs.com>
2023-03-08 14:35:53 +0800

    Cleanup code

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
+    sessions: "users/sessions",
+    registrations: "users/registrations"
   }
 
   devise_for :employees, controllers: {
-    sessions: 'employees/sessions',
-    registrations: 'employees/registrations'
+    sessions: "employees/sessions",
+    registrations: "employees/registrations"
   }
 
   authenticated :user do
