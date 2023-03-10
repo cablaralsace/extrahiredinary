3cebdb9cdef73fbe32191a60a723590d9fd21641
Alsace <alsace@narralabs.com>
2023-03-10 09:34:16 +0800

    Edit employee dashboard

--- app/controllers/users/dashboard_controller.rb
+++ app/controllers/users/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Users::DashboardController < Users::BaseController
   def index
+    @employees = Employee.all
+    # Employee::WORK_EXPERIENCE.map do |key|
+    #   [ I18n.t(".employee.work_experience.#{key}"), key]
+    # end
   end
 end
