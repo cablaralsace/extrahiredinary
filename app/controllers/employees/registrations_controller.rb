db99814215ac2b3d698b33a0c9e454c0284c307f
Alsace <alsace@narralabs.com>
2023-03-15 17:42:09 +0800

    Cleanup code

--- app/controllers/employees/registrations_controller.rb
+++ app/controllers/employees/registrations_controller.rb
@@ -15,7 +15,6 @@ class Employees::RegistrationsController < Devise::RegistrationsController
     super
   end
 
-
   # GET /resource/edit
   # def edit
   #   super
@@ -40,7 +39,7 @@ class Employees::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
