77f78a14790b88688e5b959b06ef6335b4669ec2
Alsace <alsace@narralabs.com>
2023-05-23 11:00:23 +0800

    edit modal

--- app/controllers/users/dashboard_controller.rb
+++ app/controllers/users/dashboard_controller.rb
@@ -2,4 +2,9 @@ class Users::DashboardController < Users::BaseController
   def index
     @employees = Employee.all
   end
+
+  def show
+    @employee = Employee.find(params[:id])
+    @pdf_url = url_for(@employee.file.pdf)
+  end
 end
