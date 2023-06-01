03749f8b39947b940fc296f03fbcb461018186ee
Alsace <alsace@narralabs.com>
2023-06-01 18:44:03 +0800

    add update_type method

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,9 +1,16 @@
 class Applicant < Employee
   after_create :create_initial_employment_event
+  after_save :update_type
 
   private
 
   def create_initial_employment_event
     employment_events.create!
+  end
+
+  def update_type
+    if self.hired?
+      update_column(:type, Employee.name)
+    end
   end
 end
